ec887c0d8c7924f75119cd1b55b42cd793709057
golammoula287 <golammoula287@gmail.com>
2024-11-14 15:11:27 +0600

    Image View Final Version

--- src/components/app/PropertyPage/LiveaboardDetails.jsx
+++ src/components/app/PropertyPage/LiveaboardDetails.jsx
@@ -1,59 +1,11 @@
-// import React from "react";
-// import DOMPurify from "dompurify";
-
-// function LiveaboardDetails({ propertyData, resort }) {
-//   return (
-//     <div className="flex   flex-col lg:gap-20 gap-0 lg:flex-row bg-white  sm:pb-[120px] pb-[90px]  sm:pt-[90px] pt-[75px]    customContainer  justify-center px-4 lg:px-0">
-//       <div className="flex   flex-col items-start justify-start  sm:gap-2 gap-0 font-light w-full  text-[#2f2f30]">
-//         <h1 className="  text-primary sm:text-title  text-title2 font-light    font-outfit">
-//           {resort ? (
-//             <span>The Resort</span>
-//           ) : (
-//             <span>
-//               {/* {propertyData?.nameOfProperty || propertyData?.propertyName}{" "} */}
-//               The Liveaboard
-//             </span>
-//           )}
-//         </h1>
-//         <p className="text-secondary sm:text-subtitle text-subtitle3 font-roboto font-light sm:mt-[30px] mt-[25px]">
-//           <span
-//             dangerouslySetInnerHTML={{
-//               __html: DOMPurify.sanitize(
-//                 resort
-//                   ? propertyData?.briefDescription
-//                   : propertyData?.liveABoard?.description
-//               ),
-//             }}
-//           />
-//         </p>
-//       </div>
-//       <div className="w-full  sm:mt-0 mt-[45px] aspect-[3/2] md:aspect-[3/2]">
-//         <img
-//           className="w-full h-full object-cover mt-1"
-//           src={
-//             resort
-//               ? propertyData?.featureImage
-//               : propertyData?.liveABoard?.Picture
-//           }
-//           alt="Ilike Liveaboard"
-//         />
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default LiveaboardDetails;
-
-
-
 import React from "react";
 import DOMPurify from "dompurify";
 
 function LiveaboardDetails({ propertyData, resort }) {
   return (
-    <div className="flex flex-col lg:gap-20 gap-0 lg:flex-row bg-white sm:pb-[120px] pb-[90px] sm:pt-[90px] pt-[75px] customContainer justify-center px-4 lg:px-0">
-      <div className="flex flex-col items-start justify-start sm:gap-2 gap-0 font-light w-full text-[#2f2f30]">
-        <h1 className="text-primary sm:text-title text-title2 font-light font-outfit">
+    <div className="flex   flex-col lg:gap-20 gap-0 lg:flex-row bg-white  sm:pb-[120px] pb-[90px]  sm:pt-[90px] pt-[75px]    customContainer  justify-center px-4 lg:px-0">
+      <div className="flex   flex-col items-start justify-start  sm:gap-2 gap-0 font-light w-full  text-[#2f2f30]">
+        <h1 className="  text-primary sm:text-title  text-title2 font-light    font-outfit">
           {resort ? (
             <span>The Resort</span>
           ) : (
@@ -75,15 +27,15 @@ function LiveaboardDetails({ propertyData, resort }) {
           />
         </p>
       </div>
-      <div className="w-full sm:mt-0 mt-[45px] aspect-[3/2] md:aspect-[3/2] overflow-hidden">
+      <div className="w-full  sm:mt-0 mt-[45px] aspect-[3/2] md:aspect-[3/2]">
         <img
-          className="w-full h-full object-cover"
+          className="w-full h-full object-cover mt-1"
           src={
             resort
               ? propertyData?.featureImage
               : propertyData?.liveABoard?.Picture
           }
-          alt="Liveaboard or Resort Image"
+          alt="Ilike Liveaboard"
         />
       </div>
     </div>
@@ -91,3 +43,56 @@ function LiveaboardDetails({ propertyData, resort }) {
 }
 
 export default LiveaboardDetails;
+
+
+
+
+
+
+
+
+// import React from "react";
+// import DOMPurify from "dompurify";
+
+// function LiveaboardDetails({ propertyData, resort }) {
+//   return (
+//     <div className="flex flex-col lg:gap-20 gap-0 lg:flex-row bg-white sm:pb-[120px] pb-[90px] sm:pt-[90px] pt-[75px] customContainer justify-center px-4 lg:px-0">
+//       <div className="flex flex-col items-start justify-start sm:gap-2 gap-0 font-light w-full text-[#2f2f30]">
+//         <h1 className="text-primary sm:text-title text-title2 font-light font-outfit">
+//           {resort ? (
+//             <span>The Resort</span>
+//           ) : (
+//             <span>
+//               {/* {propertyData?.nameOfProperty || propertyData?.propertyName}{" "} */}
+//               The Liveaboard
+//             </span>
+//           )}
+//         </h1>
+//         <p className="text-secondary sm:text-subtitle text-subtitle3 font-roboto font-light sm:mt-[30px] mt-[25px]">
+//           <span
+//             dangerouslySetInnerHTML={{
+//               __html: DOMPurify.sanitize(
+//                 resort
+//                   ? propertyData?.briefDescription
+//                   : propertyData?.liveABoard?.description
+//               ),
+//             }}
+//           />
+//         </p>
+//       </div>
+//       <div className="w-full sm:mt-0 mt-[45px] aspect-[3/2] md:aspect-[3/2] overflow-hidden">
+//         <img
+//           className="w-full h-full object-cover"
+//           src={
+//             resort
+//               ? propertyData?.featureImage
+//               : propertyData?.liveABoard?.Picture
+//           }
+//           alt="Liveaboard or Resort Image"
+//         />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default LiveaboardDetails;
--- src/components/app/PropertyPage/SliderModal.jsx
+++ src/components/app/PropertyPage/SliderModal.jsx
@@ -1,3 +1,142 @@
+// import * as React from "react";
+// // Import Swiper React components
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import { IoMdClose } from "react-icons/io";
+// // Import Swiper styles
+// import "swiper/css";
+// import "swiper/css/navigation";
+// // import "./styles.css";
+
+// // import required modules
+// import { Navigation } from "swiper/modules";
+
+// import Modal from "@mui/material/Modal";
+
+// function SliderModal({
+//   open,
+//   setOpen,
+//   propertyData,
+//   selectedImage,
+//   setSelectedImage,
+// }) {
+//   const [initialSlide, setInitialSlide] = React.useState(0);
+//   const handleClose = () => {
+//     setOpen(!open);
+//   };
+
+//   React.useEffect(() => {
+//     if (propertyData?.carousalImages) {
+//       const selectedIndex = propertyData.carousalImages.indexOf(selectedImage);
+//       if (selectedIndex !== -1) {
+//         setInitialSlide(selectedIndex);
+//       }
+//     }
+//   }, [selectedImage, propertyData]);
+
+//   const openHandler = (imageUrl) => {
+//     setSelectedImage(imageUrl);
+//   };
+
+//   return (
+//     <div>
+//       <Modal
+//         open={open}
+//         onClose={handleClose}
+//         aria-labelledby="modal-modal-title"
+//         aria-describedby="modal-modal-description"
+//         className="border flex items-center min-h-screen"
+//       >
+//         <div
+//           style={{ backgroundColor: "rgba(0, 0, 0, 0.7)" }}
+//           className=" w-full h-full mx-auto p-4 rounded-md   relative "
+//         >
+//           <div className="absolute right-4 top-4">
+//             <button onClick={() => setOpen(false)}>
+//               {" "}
+//               <IoMdClose className="text-white text-4xl" />
+//             </button>
+//           </div>
+//           <div className="my-4  flex justify-center w-full ">
+//             <img
+//               className=" md:w-[550px]  w-72  rounded-md"
+//               src={selectedImage}
+//             />
+//           </div>
+//           <Swiper
+//             navigation={true}
+//             modules={[Navigation]}
+//             initialSlide={initialSlide}
+//             slidesPerView={2} // Default to 2 slides per view
+//             breakpoints={{
+//               640: {
+//                 slidesPerView: 1,
+//                 spaceBetween: 0,
+//               },
+//               768: {
+//                 slidesPerView: 2,
+//                 spaceBetween: 0,
+//               },
+//               1024: {
+//                 slidesPerView: 4,
+//                 spaceBetween: 0,
+//               },
+//               1400: {
+//                 slidesPerView: 5,
+//                 spaceBetween: 0,
+//               },
+//             }}
+//           >
+//             {propertyData?.carousalImages
+//               ?.filter((image) => image !== selectedImage)
+//               ?.map((image, index) => (
+//                 <SwiperSlide
+//                   onClick={() => openHandler(image)}
+//                   key={`other-${index}`}
+//                 >
+//                   <div className="aspect-w-3 aspect-h-2 ">
+//                     <img
+//                       className="h-full w-full cursor-pointer object-cover"
+//                       src={image}
+//                     />
+//                   </div>
+//                 </SwiperSlide>
+//               ))}
+
+//             {propertyData?.carousal
+//               ?.filter((image) => image !== selectedImage)
+//               ?.map((image, index) => (
+//                 <SwiperSlide
+//                   onClick={() => openHandler(image)}
+//                   key={`other-${index}`}
+//                 >
+//                   <div className="aspect-w-3 aspect-h-2">
+//                     <img
+//                       className="h-full w-full cursor-pointer object-cover"
+//                       src={image}
+//                     />
+//                   </div>
+//                 </SwiperSlide>
+//               ))}
+//           </Swiper>
+//         </div>
+//       </Modal>
+//     </div>
+//   );
+// }
+
+// export default SliderModal;
+
+
+
+
+
+
+
+{/*Image View Final Version */}
+
+
+
+
 import * as React from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -56,68 +195,93 @@ function SliderModal({
               <IoMdClose className="text-white text-4xl" />
             </button>
           </div>
-          <div className="my-4  flex justify-center w-full ">
+          {/* Main Image View */}
+          <div className="my-4 flex justify-center w-full">
+            <div
+              className="bg-gray-200 rounded-md overflow-hidden"
+              style={{ width: "550px", height: "400px" }}
+            >
+              <img
+                className="w-full h-full object-contain"
+                src={selectedImage}
+                alt="Selected"
+              />
+            </div>
+          </div>
+        
+<div className="swiper-container" style={{ position: 'absolute', bottom: 0, left: 0, right: 0 }}>
+  <Swiper
+    navigation={true}
+    modules={[Navigation]}
+    initialSlide={initialSlide}
+    slidesPerView={2}
+    breakpoints={{
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 0,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 0,
+      },
+      1400: {
+        slidesPerView: 5,
+        spaceBetween: 0,
+      },
+    }}
+  >
+    {propertyData?.carousalImages
+      ?.filter((image) => image !== selectedImage)
+      ?.map((image, index) => (
+        <SwiperSlide
+          onClick={() => openHandler(image)}
+          key={`carousalImage-${index}`}
+        >
+          <div className="aspect-w-3 aspect-h-2">
             <img
-              className=" md:w-[550px]  w-72  rounded-md"
-              src={selectedImage}
+              className="h-full w-full cursor-pointer object-cover"
+              src={image}
             />
           </div>
-          <Swiper
-            navigation={true}
-            modules={[Navigation]}
-            initialSlide={initialSlide}
-            slidesPerView={2} // Default to 2 slides per view
-            breakpoints={{
-              640: {
-                slidesPerView: 1,
-                spaceBetween: 0,
-              },
-              768: {
-                slidesPerView: 2,
-                spaceBetween: 0,
-              },
-              1024: {
-                slidesPerView: 4,
-                spaceBetween: 0,
-              },
-              1400: {
-                slidesPerView: 5,
-                spaceBetween: 0,
-              },
-            }}
-          >
-            {propertyData?.carousalImages
-              ?.filter((image) => image !== selectedImage)
-              ?.map((image, index) => (
-                <SwiperSlide
-                  onClick={() => openHandler(image)}
-                  key={`other-${index}`}
-                >
-                  <div className="aspect-w-3 aspect-h-2 ">
-                    <img
-                      className="h-full w-full cursor-pointer object-cover"
-                      src={image}
-                    />
-                  </div>
-                </SwiperSlide>
-              ))}
+        </SwiperSlide>
+      ))}
 
-            {propertyData?.carousal
-              ?.filter((image) => image !== selectedImage)
-              ?.map((image, index) => (
-                <SwiperSlide
-                  onClick={() => openHandler(image)}
-                  key={`other-${index}`}
-                >
-                  <div className="aspect-w-3 aspect-h-2">
-                    <img
-                      className="h-full w-full cursor-pointer object-cover"
-                      src={image}
-                    />
-                  </div>
-                </SwiperSlide>
-              ))}
-          </Swiper>
+    {propertyData?.carousal
+      ?.filter((image) => image !== selectedImage)
+      ?.map((image, index) => (
+        <SwiperSlide
+          onClick={() => openHandler(image)}
+          key={`carousal-${index}`}
+        >
+          <div className="aspect-w-3 aspect-h-2">
+            <img
+              className="h-full w-full cursor-pointer object-cover"
+              src={image}
+            />
+          </div>
+        </SwiperSlide>
+      ))}
+
+    {propertyData?.featureImage && (
+      <SwiperSlide
+        onClick={() => openHandler(propertyData.featureImage)}
+        key="featureImage"
+      >
+        <div className="aspect-w-3 aspect-h-2">
+          <img
+            className="h-full w-full cursor-pointer object-cover"
+            src={propertyData.featureImage}
+          />
+        </div>
+      </SwiperSlide>
+    )}
+  </Swiper>
+</div>
+
         </div>
       </Modal>
     </div>
@@ -125,6 +289,5 @@ function SliderModal({
 }
 
 export default SliderModal;
-
 
 
